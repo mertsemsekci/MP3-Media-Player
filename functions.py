248dee599abafcb0f2c40fd7d7b32ffc2cc09f75
Abdurrahman Mert Semsekçi <semsekcimert@gmail.com>
2024-03-15 17:08:23 +0300

    Time slider added

--- functions.py
+++ functions.py
@@ -2,17 +2,20 @@ from tkinter import filedialog
 import pygame
 
 # Function to choose a file
-def choose_file(time_label):
+def choose_file(time_label, time_scale):
     file_path = filedialog.askopenfilename(filetypes=[("MP3 files", "*.mp3")])
     if file_path:
-        play_music(file_path, time_label)
+        play_music(file_path, time_label, time_scale)
 
 # Function to play music
-def play_music(file_path, time_label):
+def play_music(file_path, time_label, time_scale):
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
     update_time_label(time_label)
+    duration = pygame.mixer.Sound(file_path).get_length()
+    time_scale.config(to=duration)
+    time_scale.pack(pady=10)
 
 # Function to pause music playback
 def pause_music():
@@ -49,3 +52,11 @@ def update_time_label_helper(milliseconds):
         minutes_str = "00:"
         seconds_str = f"00"
     return minutes_str + seconds_str
+
+# Function to set music time
+def set_music_time(value):
+    pygame.mixer.music.set_pos(value)
+
+# Function to handle slider change
+def on_slider_changed(value):
+    set_music_time(value)
--- player.py
+++ player.py
@@ -1,12 +1,12 @@
 import tkinter as tk
-from functions import choose_file, pause_music, resume_music, stop_music, adjust_volume, update_time_label
+from functions import choose_file, pause_music, resume_music, stop_music, adjust_volume, set_music_time
 
 # Create a tkinter application
 root = tk.Tk()
 root.title("Media Player")
 
 # File selection button
-choose_button = tk.Button(root, text="Select Music", command=lambda: choose_file(time_label))
+choose_button = tk.Button(root, text="Select Music", command=lambda: choose_file(time_label, time_scale))
 choose_button.pack(pady=20)
 
 # Add control buttons
@@ -19,6 +19,7 @@ resume_button.pack(pady=10)
 stop_button = tk.Button(root, text="Stop", command=stop_music)
 stop_button.pack(pady=10)
 
+# Volume slider
 volume_slider = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL, command=lambda value: adjust_volume(int(value)))
 volume_slider.set(100)
 volume_slider.pack(pady=10)
@@ -26,5 +27,12 @@ volume_slider.pack(pady=10)
 # Duration displayer
 time_label = tk.Label(root, text="00:00", font=("Arial", 12))
 time_label.pack(pady=10)
+
+# Time slider
+time_scale = tk.Scale(root, from_=0, to=0, orient=tk.HORIZONTAL)
+time_scale.pack(pady=10)
+
+# Bind the time slider to set music time
+time_scale.bind("<ButtonRelease-1>", lambda event: set_music_time(time_scale.get()))
 
 root.mainloop()
